adb0095f47b3cb09ac690ba0e33b322b44b1fa08
Lucas chagas <lucazchagaz@outlook.com>
2023-09-06 16:13:28 -0300

    Feat: add ResStyle library

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text} from './src/components/Text';
+import {Button} from './src/components/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text presets="headingLarge" style={{fontFamily: 'Satoshi-Black'}}>
-        Seja bem vindo
-      </Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text presets="headingLarge" style={{fontFamily: 'Satoshi-Black'}}>
+          Seja bem vindo
+        </Text>
+        <Button mt="s56" mb="s20" title="Entrar" />
+        <Button isLoading />
+      </View>
+    </ThemeProvider>
   );
 }
 
@@ -16,6 +23,7 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     flex: 1,
+    paddingHorizontal: 10,
   },
 });
 
