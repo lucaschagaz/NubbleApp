d8a3962cc21fe900b37e55c15b8a19d25267f66e
Lucas chagas <lucazchagaz@outlook.com>
2023-09-18 21:56:52 -0300

    Feat: added react native svg lib and svgs files

--- App.tsx
+++ App.tsx
@@ -4,16 +4,21 @@ import {Text} from './src/components/Text';
 import {Button} from './src/components/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+import {EyeOff} from './src/assets/icons/EyeOff';
+import {EyeOn} from './src/assets/icons/EyeOn';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <View style={styles.container}>
-        <Text presets="headingLarge" style={{fontFamily: 'Satoshi-Black'}}>
-          Seja bem vindo
-        </Text>
-        <Button mt="s56" mb="s20" title="Entrar" />
+        <Text preset="headingLarge">Seja bem vindo</Text>
+        <Button variant="outline" mt="s20" mb="s20" title="Entrar" />
+        <Button mb="s20" title="Entrar" />
         <Button isLoading />
+        {/* <EyeOff size={50} color="red" />
+        <EyeOn size={50} color="red" />
+        <Button disabled variant="outline" mt="s20" mb="s20" title="Entrar" />
+        <Button disabled variant="primary" mb="s20" title="Entrar" /> */}
       </View>
     </ThemeProvider>
   );
